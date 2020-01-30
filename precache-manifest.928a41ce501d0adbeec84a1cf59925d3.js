07142e8939d765b8de301ce1586d655a4825c318
Ilumiant <antonio@ilumiware.com>
2020-01-30 06:07:19 -0400

    Pagina para publicar

---
+++ precache-manifest.928a41ce501d0adbeec84a1cf59925d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63ef1e70250f7ab491fb8c047296615",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7856c6f2bc13a7a688c2",
+    "url": "/static/css/main.137c22b7.chunk.css"
+  },
+  {
+    "revision": "9ab69fda1139e6f1542f",
+    "url": "/static/js/2.234850c0.chunk.js"
+  },
+  {
+    "revision": "7856c6f2bc13a7a688c2",
+    "url": "/static/js/main.8f0147ff.chunk.js"
+  },
+  {
+    "revision": "5dbf5c6566794130bf02",
+    "url": "/static/js/runtime~main.bec474ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
